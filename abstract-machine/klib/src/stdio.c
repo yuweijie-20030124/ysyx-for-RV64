50b1cd46655e513ac476602ff7caf7d1400b3aad
yuweijie <479634193@qq.com>
2026-08-26 01:33:59 +0800

    klib && successfully 用户程序

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,101 +1,98 @@
+#include <am.h>
 #include <klib.h>
+#include <klib-macros.h>
+#include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+static char buf[1024];
+
+void putch(char ch);
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+    va_list ap;
+    va_start(ap, fmt);
+    
+    int val = vsnprintf(buf, 1024, fmt, ap);
+    char *tmp = buf;
+    while (*tmp != 0) {
+        putch(*tmp);
+        tmp++;
+    }
+    va_end(ap);
+    
+    return val;
 }
 
-int vprintf(const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+      panic("Not implemented");
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  
+  int val = vsnprintf(out, 1024, fmt, ap);
+  va_end(ap);
+
+  return val;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
-}
-
+//out   是指向数组的指针
+//n     写入字符串的最大数量
+//fmt   格式字符串，%s %d啥的
+//ap    包含了要格式化的可变数量的参数。这个列表是通过 va_start 宏初始化的，可以通过 va_arg 宏来访问其中的参数。
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-int __am_vsscanf_internal(const char *str, const char **end_pstr, const char *fmt, va_list ap) {
-  const char *pstr = str;
-  const char *pfmt = fmt;
-  int item = -1;
-  while (*pfmt) {
-    char ch = *pfmt ++;
-    if (isspace(ch)) {
-      for (ch = *pfmt; isspace(ch); ch = *(++ pfmt));
-      for (ch = *pstr; isspace(ch); ch = *(++ pstr));
-      item ++;
-      continue;
-    }
-    switch (ch) {
-      case '%': break;
-      default:
-        if (*pstr == ch) { // match
-          pstr ++;
-          item ++;
-          continue;
+    char *start = out;
+    while (n-- && *fmt != '\0') {
+        if (*fmt == '%') {
+            fmt++;
+            if (*fmt == 's') {
+                char *tmp_s = va_arg(ap, char*);
+                while (*tmp_s != '\0') {
+                    *out++ = *tmp_s++;
+                }
+            }
+            else if (*fmt == 'd') {
+                int tmp_int = va_arg(ap, int);//从可变参量列表ap中获取int类型的参数
+                if (tmp_int < 0) {
+                    *out++ = '-';
+                    tmp_int = -1 * tmp_int;
+                }
+                int number = tmp_int;
+                int len  = 0;
+                do {
+                    number /= 10; //看这个%d一共有几位
+                    len++;
+                } while (number);
+                out = out + len - 1;//定位输出位置
+                int tmp_len = len;  //逐位写入数字字符
+                while (tmp_len--) {
+                    int tmp = tmp_int % 10;
+                    *out-- = tmp + 48; //将数字转换为对应的ASCII字符
+                    tmp_int /= 10;  //准备处理下一个字符
+                }
+                out += (len+1);
+            }
+            else if (*fmt == '%') {
+                *out++ = '%';
+            }
+            else if (*fmt == 'c') {
+                char tmp_char = va_arg(ap, int);
+                *out++ = tmp_char;
+            }
+            else {
+                return -1;
+            }
         }
-        goto end; // fail
+        else {
+            *out++ = *fmt;
+        }
+        fmt++;
     }
-
-    char *p;
-    ch = *pfmt ++;
-    switch (ch) {
-      // conversion specifier
-      case 'd':
-        *(va_arg(ap, int *)) = strtol(pstr, &p, 10);
-        if (p == pstr) goto end; // fail
-        pstr = p;
-        item ++;
-        break;
-
-      case 'c':
-        *(va_arg(ap, char *)) = *pstr ++;
-        item ++;
-        break;
-
-      default:
-        printf("Unsupported conversion specifier '%c'\n", ch);
-        assert(0);
-    }
-  }
-
-end:
-  if (end_pstr) {
-    *end_pstr = pstr;
-  }
-  return item;
-}
-
-int vsscanf(const char *str, const char *fmt, va_list ap) {
-  return __am_vsscanf_internal(str, NULL, fmt, ap);
-}
-
-int sscanf(const char *str, const char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  int r = vsscanf(str, fmt, ap);
-  va_end(ap);
-  return r;
-}
-
-int __isoc99_sscanf(const char *str, const char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  int r = vsscanf(str, fmt, ap);
-  va_end(ap);
-  return r;
+    *out = '\0';
+    return out - start;
 }
 
 #endif
